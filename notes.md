570f5cd3fb9714c51d365fa34d962dd0801b0a5f
magalipujol <magalicpujol@gmail.com>
2021-12-06 09:37:53 -0300

    finished with testing

--- notes.md
+++ notes.md
@@ -1,5 +1,20 @@
 # C# and .Net Fundamentals
 
-- (CLR means Common Language Runtime) .Net is a runtime, which means that it provides a space for the application to run. It know how to bring the program to life, manage the memory and communcate with the OS.
+- (CLR means Common Language Runtime) .Net is a runtime, which means that it provides a space for the application to run. It know how to bring the program to life, manage the memory and communicate with the OS.
 - (FCL means Framework Class Library). In addition to a runtime, .Net also provides a set of libraries.
 - The solution file is a file that contains all the references to the files that I want to run tests on.
+- There is a difference between passing a parameter by reference and by value. Passing by reference a variable means that you're passing the location of memory of the variable, and passing by value means that the value of the variable is being copied, if it's a primitive type it copies the value itself, but if not, it copies the pointer.
+- To know the type of a variable (value or reference), press <kbd>F12</kbd>, and if it's defined as a 'struct', it's a value type, and if it's defined as a 'class', it's a reference type.
+For example, Boolean is a value type of C#, and bool is the .Net shortcut.
+- .Net has a garbage collector, that keeps that of all the object and variables I create, and knows when there's no variables and no fields that are pointing to or using that object, and cleans up that space. So I don't have to tell .Net that i'm finished using an object and tell the runtime to delete that object of free up the memory.
+
+- In order to run the code, I have to be in the folder of the project and press <kbd>Ctrl</kbd> + <kbd>F5</kbd> or execute the following command
+~~~
+$ dotnet run
+~~~
+and to run the tests in the solution file, execute
+~~~
+$ dotnet test
+~~~
+
+
--- test/Gradebook.tests/TypeTests.cs
+++ test/Gradebook.tests/TypeTests.cs
@@ -4,16 +4,87 @@ namespace Gradebook.tests;
 
 public class TypeTests
 {
-    [Fact]
-    public void Test1()
-    {
-        var book1 = GetBook("Book 1");
-        var book2 = GetBook("Book 2");
+    public void DoNothing() { }
 
+    [Fact]
+    public void CanPassByReferenceValueTypes()
+    {
+        var x = GetInt();
+        SetInt(ref x);
+        Assert.Equal(42, x);
+    }
+
+    [Fact]
+    public void StringBehaveLikeValueTypes()
+    {
+        string something = "gugu";
+        var upper = MakeUppercase(something);
+        Assert.Equal("gugu", something);
+        Assert.Equal("GUGU", upper);
+
+    }
+
+    public string MakeUppercase(string smt) {
+        // The method doesn't change the string invoked on, it returns a copy of the string converted to upper
+        return smt.ToUpper();
+    }
+    public void SetInt(ref int x) {
+        x = 42;
+           }
+    public int GetInt() {
+        return 3;
+    }
+
+    [Fact]
+    public void CanPassByReferenceAValue()
+    {
+        var book1 = GetBook("Student 1");
+        GetBookSetName(ref book1, "Generic name");
+
+        Assert.Equal("Generic name", book1.Student);
+    }
+    [Fact]
+    public void TwoVariablesCanReferenceTheSameObject()
+    {
+        var book1 = GetBook("Student1");
+        // book2 and book1 contain the same pointer to an object
+        var book2 = book1;
+
+        Assert.Same(book1, book2);
+        // The line above is the same as:
+        // Assert.True(Object.ReferenceEquals(book1, book2))
+        // It checks if both variables point to the same location of memory
+        // they reference the same object
+    }
+    [Fact]
+    public void CanSetNameFromReference()
+    {
+        var book1 = GetBook("Student 1");
+        SetName(book1, "new student");
+
+        Assert.Equal("new student", book1.Student);
+    }
+    [Fact]
+    public void GetBookReturnsDifferentObjects()
+    {
+        var book1 = GetBook("Student 1");
+        var book2 = GetBook("Student 2");
+
+        Assert.Equal("Student 1", book1.Student);
+        Assert.Equal("Student 2", book2.Student);
     }
 
     private Book GetBook(string name)
     {
         return new Book(name);
+    }
+
+    private void GetBookSetName(ref Book book, string name) {
+        book = new Book(name);
+    }
+
+    private void SetName(Book book, string name)
+    {
+        book.Student = name;
     }
 }
